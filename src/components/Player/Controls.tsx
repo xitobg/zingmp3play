4ded4c7dcfac7faad623db96ac8c3264c7f2cee0
phamhiep2506 <phamhiep25062k@gmail.com>
2022-06-20 21:30:20 +0700

    update icon player

--- src/components/Player/Controls.tsx
+++ src/components/Player/Controls.tsx
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import IconPrevious from "../Icons/Previous"
 import IconPlay from "../Icons/Play"
 import IconNext from "../Icons/Next"
+import IconLyric from "../Icons/Lyric"
+import IconRepeat from "../Icons/Repeat"
+import IconShuffle from "../Icons/Shuffle"
+import IconVolume from "../Icons/Volume"
+import IconVolumeMute from "../Icons/VolumeMute"
+import IconVolumeHalf from "../Icons/VolumeHalf"
+import IconArrowUp from "../Icons/ArrowUp"
+
 interface controlsProps {
   thumbnail: string
   title: string
@@ -11,6 +19,37 @@ interface controlsProps {
 const Controls:React.FC<controlsProps> = ({ thumbnail, title, artistsNames }) => {
 
   const styleButtons = "p-2 flex justify-center items-center bg-transparent rounded-[25%] transition-all duration-200 hover:bg-[color:var(--color-secondary-bg-for-transparent)]"
+
+  const [isActiveDotSlider, setActiveDotSlider] = useState<boolean>(false)
+  const [valueVolumeSlider, setValueVolumeSlider] = useState<number>(50)
+
+  const handleActiveDotSlider = (handle: boolean) => {
+    setActiveDotSlider(handle)
+  }
+
+  const handleChangeVolumeSlider = (event: any) => {
+    setValueVolumeSlider(event.target.value)
+  }
+
+  let volumeUI
+  if (valueVolumeSlider == 0) {
+    volumeUI =
+      <button className={"mx-2 my-0 " + styleButtons} title="Mute">
+        <IconVolumeMute setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+      </button>
+  }
+  if(valueVolumeSlider > 0) {
+    volumeUI =
+      <button className={"mx-2 my-0 " + styleButtons} title="Mute">
+        <IconVolumeHalf setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+      </button>
+  }
+  if(valueVolumeSlider > 50) {
+    volumeUI =
+      <button className={"mx-2 my-0 " + styleButtons} title="Mute">
+        <IconVolume setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+      </button>
+  }
 
   return (
     <div className="grid grid-cols-3 h-full mx-[10vw] z-[-1]">
@@ -35,21 +74,50 @@ const Controls:React.FC<controlsProps> = ({ thumbnail, title, artistsNames }) =>
 
       {/* Middlen Controls Button */}
       <div className="flex justify-center items-center">
-        <button className={"mx-2 my-0 " + styleButtons}>
+        <button className={"mx-2 my-0 " + styleButtons} title="Previous Song">
           <IconPrevious setColor="white" setWidth="16px" setHeight="16px" />
         </button>
-        <button className={"w-[42px] h-[42px] mx-2 my-0 " + styleButtons}>
+        <button className={"w-[42px] h-[42px] mx-2 my-0 " + styleButtons} title="Play Song">
           <IconPlay setColor="white" setWidth="24px" setHeight="24px"/>
         </button>
-        <button className={"mx-2 my-0 " + styleButtons}>
+        <button className={"mx-2 my-0 " + styleButtons} title="Next Song">
           <IconNext setColor="white" setWidth="16px" setHeight="16px" />
         </button>
       </div>
       {/* End Middlen Controls Button */}
 
       {/* Right Controls Button */}
-      <div>
-
+      <div className="flex justify-center items-center">
+        <button className={"mx-2 my-0 " + styleButtons} title="Lyric & Karaoke">
+          <IconLyric setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+        </button>
+        <button className={"mx-2 my-0 " + styleButtons} title="Repeat">
+          <IconRepeat setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+        </button>
+        { volumeUI }
+        <button className={"mx-2 my-0 " + styleButtons} title="Shuffle">
+          <IconShuffle setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+        </button>
+        <div
+          className="flex justify-center py-[6px]"
+          onMouseOver={ () => handleActiveDotSlider(true)}
+          onMouseOut={ () => handleActiveDotSlider(false)}
+        >
+          <input
+            type="range"
+            className={"volume__slider h-1 w-[84px] rounded-[5px] " + (isActiveDotSlider ? "volume__slider--active" : "") }
+            min="0"
+            max="100"
+            value={valueVolumeSlider}
+            style={{
+              background: `linear-gradient(90deg, var(--color-primary) ${valueVolumeSlider}%, var(--color-player-slider) ${valueVolumeSlider}%)`
+            }}
+            onChange={handleChangeVolumeSlider}
+          />
+        </div>
+        <button className={"mx-2 my-0 " + styleButtons} title="Volume">
+          <IconArrowUp setColor="var(--color-text)" setWidth="16px" setHeight="16px" />
+        </button>
       </div>
       {/* End Right Controls Button */}
     </div>
