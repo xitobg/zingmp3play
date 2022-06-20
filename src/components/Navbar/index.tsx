473c38f164c845c09f8953c0f61d568a9048367d
phamhiep2506 <phamhiep25062k@gmail.com>
2022-06-20 16:57:37 +0700

    remove user navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,23 +1,9 @@
-import React, { useState } from "react"
+import React from "react"
 import Search from "./SearchBox"
 import NavArrows from "./NavArrows"
 import NavLinks from "./NavLinks"
-import IconSetting from "../Icons/Setting"
-import IconLogin from "../Icons/Login"
-import IconGithub from "../Icons/Github"
-import { Link } from "react-router-dom"
 
 const Navbar: React.FC = () => {
-
-  // Style Item Context Menu
-  const styleItemsContextMenu = "flex items-center font-semibold px-[5px] py-[8px] rounded-[8px] hover:bg-[color:var(--color-primary-bg-for-transparent)]"
-
-  const [isContextMenuActive, setContextMenuActive] = useState<boolean>(false)
-
-  const handleToggleContextMenu = () => {
-    setContextMenuActive(!isContextMenuActive)
-  }
-
   return (
     <nav className="flex justify-between items-center bg-[color:var(--color-navbar-bg)] h-16 px-[10vw] inset-0 fixed z-50 backdrop-blur-[20px] backdrop-saturate-[180%]">
       {/* Navigation Buttons Arrow */}
@@ -33,49 +19,9 @@ const Navbar: React.FC = () => {
       </div>
       {/* End Navigation Links */}
 
-      {/* Right Part */}
-      <div className="flex items-center">
-
-        {/* Search Box */}
-        <Search />
-        {/* End Search Box */}
-
-        <div className="relative">
-          {/* Avatar */}
-          <img
-            src="http://s4.music.126.net/style/web2/img/default/default_avatar.jpg"
-            alt="avatar"
-            className="h-[30px] ml-3 rounded-full cursor-pointer hover:brightness-[80%]"
-            onClick={handleToggleContextMenu}
-          />
-          {/* End Avatar */}
-
-          {/* Context Menu */}
-          <div
-            className={`absolute right-0 min-w-[136px] max-w-[240px] bg-[#282828] text-[color:var(--color-text)] rounded-xl p-[6px]
-              ${
-                (isContextMenuActive === true ? "block" : "hidden")
-              }
-            `}
-          >
-            <Link to="/setting" className={styleItemsContextMenu}>
-              <IconSetting setColor="white" setWidth="16px" setHeight="16px" />
-              <b className="pl-1">Setting</b>
-            </Link>
-            <Link to="/login" className={styleItemsContextMenu}>
-              <IconLogin setColor="white" setWidth="16px" setHeight="16px" />
-              <b className="pl-1">Login</b>
-            </Link>
-            <hr className="my-[4px] mx-[10px]" />
-            <a href="https://github.com/phamhiep2506/music-player" className={styleItemsContextMenu} target="_blink">
-              <IconGithub setColor="white" setWidth="16px" setHeight="16px" />
-              <b className="pl-1">Github Repo</b>
-            </a>
-          </div>
-          {/* Context Menu */}
-        </div>
-      </div>
-      {/* End Right Part */}
+      {/* Search Box */}
+      <Search />
+      {/* End Search Box */}
     </nav>
   )
 }
--- src/components/Player/ProgressBar.tsx
+++ src/components/Player/ProgressBar.tsx
@@ -3,10 +3,15 @@ import React, { useState } from "react"
 const ProgressBar:React.FC = () => {
 
   const [isActiveDotSlider, setActiveDotSlider] = useState<boolean>(false)
+  const [isActiveTooltipSlider, setActiveTooltipSlider] = useState<boolean>(false)
   const [valueSlider, setValueSlider] = useState<number>(0)
 
   const handleActiveDotSlider = (handle: boolean) => {
     setActiveDotSlider(handle)
+  }
+
+  const handleActiveTooltipSlider = (handle: boolean) => {
+    setActiveTooltipSlider(handle)
   }
 
   const handleChangeValueSlider = (event: any) => {
@@ -17,13 +22,11 @@ const ProgressBar:React.FC = () => {
     // Progress Bar
     <div
       className="my-[-6px] w-full"
-      onMouseOver={() => handleActiveDotSlider(true)}
-      onMouseOut={() => handleActiveDotSlider(false)}
+      onMouseOver={ () => { handleActiveDotSlider(true); handleActiveTooltipSlider(true) }}
+      onMouseOut={ () => { handleActiveDotSlider(false); handleActiveTooltipSlider(false) }}
     >
       {/* Progress Bar Slider */}
-      <div
-        className="flex w-auto py-[6px]"
-      >
+      <div className="flex w-auto py-[6px] relative">
         <input
           className={ "player__slider w-full h-[2px] rounded-[5px] " + (isActiveDotSlider ? "player__slider--active" : "") }
           type="range"
@@ -35,8 +38,22 @@ const ProgressBar:React.FC = () => {
           }}
           onChange={handleChangeValueSlider}
         />
+
+        {/* Tooltip */}
+        <div
+          className={"absolute -top-8 -translate-x-2/4 " + (isActiveTooltipSlider ? "visible" : "invisible")}
+          style={{
+            left: `${valueSlider}%`
+          }}
+        >
+          <div className="text-sm font-medium whitespace-nowrap px-[6px] py-[2px] min-w-[20px] text-center text-[#000] rounded-[5px] bg-[#fff] box-content">
+            <span>3:01</span>
+          </div>
+        </div>
+        {/* End Tooltip */}
       </div>
       {/* End Progress Bar Slider */}
+
     </div>
     // End Progress Bar
   )
